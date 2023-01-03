eb75c76702df062d8759da1ad59fe72355500628
PatricioRubial <91898476+PatricioRubial@users.noreply.github.com>
2023-01-03 10:28:28 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialComponent } from './components/social/social.component';
+import { FotoPerfilComponent } from './components/foto-perfil/foto-perfil.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { SkilsComponent } from './components/skils/skils.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    LogoAPComponent,
+    HeaderComponent,
+    SocialComponent,
+    FotoPerfilComponent,
+    EstudiosComponent,
+    SkilsComponent
   ],
   imports: [
     BrowserModule,
