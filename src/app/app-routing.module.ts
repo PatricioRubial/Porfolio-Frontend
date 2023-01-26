6a2c5eb2c224fa6e09cbbd02c5cccf42bd09253a
PatricioRubial <91898476+PatricioRubial@users.noreply.github.com>
2023-01-26 11:08:33 -0300

    Loading-Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { ProfileEditComponent } from './components/acerca-de/profile-edit.compon
 import { StudyEditComponent } from './components/estudios/study-edit.component';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
 import { LoginComponent } from './components/login/login.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-
 import { ProjectsEditComponent } from './components/proyectos/projects-edit.component';
 import { SkillEditComponent } from './components/skils/skill-edit.component';
 
